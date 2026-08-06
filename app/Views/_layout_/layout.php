1b768c4443634276b9adf4441c842940ae55c802
M K <mammadkouchenani@gmail.com>
2026-08-06 04:15:12 +0330

    goftino customer support chat added.

--- app/Views/_layout_/layout.php
+++ app/Views/_layout_/layout.php
@@ -61,6 +61,10 @@
 <?= $this->include('_layout_/partials/scripts') ?>
 <?= $this->renderSection('scripts') ?>
 
+<script type="text/javascript">
+  ["keydown","touchmove","touchstart","mouseover"].forEach(function(v){window.addEventListener(v,function(){if(!window.isGoftinoAdded){window.isGoftinoAdded=1;var i="zN9Asj",d=document,g=d.createElement("script"),s="https://www.goftino.com/widget/"+i,l=localStorage.getItem("goftino_"+i);g.type="text/javascript",g.async=!0,g.src=l?s+"?o="+l:s;d.getElementsByTagName("head")[0].appendChild(g);}})});
+</script>
+
 </body>
 
 </html>
